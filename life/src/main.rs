de2d2f322c9cd782749eacecd01ed15963a6416b
Diego Civini <diego.civini@lambdaclass.com>
2025-03-14 11:56:09 -0300

    Add documentation

--- life/src/main.rs
+++ life/src/main.rs
@@ -5,20 +5,61 @@ const OFFSETS: [i8; 3] = [-1, 0, 1];
 const DEAD: u8 = 0;
 const ALIVE: u8 = 1;
 
+/// Verifies if the position represented by the point ('row_i', 'col_i') is inside
+/// the matrix. 
+/// 
+/// # Arguments
+/// 
+/// - `row_i`: Represents the row in the matrix of the point.
+/// - `col_i`: Represents the column in the matrix of the point.
+/// 
+/// # Returns
+/// 
+/// A `bool` indicating if this is true or not.
 fn is_position_valid(row_i: i8, col_i: i8) -> bool {
     (row_i >= 0 && row_i < N as i8) && (col_i >= 0 && col_i < N as i8)
 }
 
+/// Verifies if 2 points on the matrix are the same point. 
+/// 
+/// # Arguments
+/// 
+/// - `row_i_1`: Represents the row in the matrix of point 1.
+/// - `col_i_1`: Represents the column in the matrix of point 1.
+/// - `row_i_2`: Represents the row in the matrix of point 2.
+/// - `col_i_2`: Represents the column in the matrix of point 2.
+/// 
+/// # Returns
+/// 
+/// A `bool` indicating if this is true or not.
 fn is_different_cell(row_i_1: i8, col_i_1: i8, row_i_2: i8, col_i_2: i8) -> bool {
     row_i_1 != row_i_2 || col_i_1 != col_i_2
 }
 
+/// Changes the state of specific cells in the matrix.
+/// 
+/// # Arguments
+/// 
+/// - `cells`: A Vec which contains tuples that represent the cells in the amtrix to modify.
+/// - `state`: Represents wether the cell is alive or dead.
+/// - `matrix`: An array with arrays that represent the matrix which contains every cell.
 fn manage_cell_state(cells: Vec<(usize, usize)>, state: u8, matrix: &mut [[u8; N]; N]) {
     for (row_i, col_i) in cells {
         matrix[row_i][col_i] = state;
     }
 }
 
+/// Checks the eight neighbours of a cell and counts how many of them are alive.
+/// 
+/// # Arguments
+/// 
+/// - `col_i_1`: Represents the column in the matrix of the cell.
+/// - `row_i_1`: Represents the row in the matrix of the cell.
+/// - `matrix`: An array with arrays that represent the matrix which contains every cell.
+/// 
+/// # Returns
+/// 
+/// An `i32` that represents the number of alive neighbours.
 fn check_cell_alive_neighbours(col_i: usize, row_i: usize, matrix: [[u8; N]; N]) -> i32 {
     let mut alive_neighbours = 0;
     // This parsing is done here so we don't do it in every loop 
@@ -38,7 +79,7 @@ fn check_cell_alive_neighbours(col_i: usize, row_i: usize, matrix: [[u8; N]; N])
 
             let cell_to_check = matrix[row_to_check as usize][col_to_check as usize];
 
-            if cell_to_check == 1 {
+            if cell_to_check == ALIVE {
                 alive_neighbours += 1;
             }
         }
